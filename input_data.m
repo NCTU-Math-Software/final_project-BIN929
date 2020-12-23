f811be7470148bed82de130ec6c6f57c19bb1d2b
BIN929 <71300195+BIN929@users.noreply.github.com>
2020-12-23 18:16:54 +0800

    Update input_data.m

--- input_data.m
+++ input_data.m
@@ -5,20 +5,20 @@ function [N ,bomb_num] = input_data
     %     炸彈的算法是最低我們給他們最少可以放10顆
     %     最多可以放（（N*N）/ 2） 顆炸彈
     
-    N = input('請輸入想要玩N*N的大小的N( 最低9*9 最高16*16 ):');
+    N = input('Please enter the size of N you want to play N*N (minimum 9*9 maximum 16*16):');
     %   如果輸入低於9或高於16就需要玩家再次輸入
     while( N < 9 || N > 16 )                                        
-        N = input('請輸入想要玩N*N的大小的N( 最低9*9 最高16*16 ):');
+        N = input('Please enter the size of N you want to play N*N (minimum 9*9 maximum 16*16):');
     end
     
     bomb_min = 10;
     bomb_max = round((N*N)/2);
-    fprintf('請輸入 %d 到 %d 之間的炸彈數量\n',bomb_min,bomb_max);
-    bomb_num = input('幾顆炸彈:');
+    fprintf('Please enter the number of bombs between %d and %d \n',bomb_min,bomb_max);
+    bomb_num = input('number of bombs:');
     %   如果輸入低於 bomb_min 或高於 bomb_max 就需要玩家再次輸入
     while (bomb_num < bomb_min || bomb_num > bomb_max)
-        fprintf('請輸入 %d 到 %d 之間的炸彈數量\n',bomb_min,bomb_max);
-        bomb_num = input('炸彈數量:');
+        fprintf('Please enter the number of bombs between %d and %d \n',bomb_min,bomb_max);
+        bomb_num = input('number of bombs:');
     end
     
 end
